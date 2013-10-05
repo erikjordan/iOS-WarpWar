f6a5b95ba555842bd57bece882507e949135e8b9
Erik Jordan <erik@jordan.name>
2013-10-04 21:29:43 -0500

    Initial Commit

---
+++ iOS WarpWar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS WarpWar
+//
+//  Created by Erik Jordan on 10/4/13.
+//  Copyright (c) 2013 Erik Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS WarpWar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOS WarpWar
+//
+//  Created by Erik Jordan on 10/4/13.
+//  Copyright (c) 2013 Erik Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
